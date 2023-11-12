61f1a96a0c19d3dcca78b572e1c8886ac47c181e
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-11-12 14:35:02 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -30,6 +30,7 @@ project {
 
     buildType(Build1)
      buildType(Build2)
+buildType(Build3)
 }
 
 object Build1 : BuildType({
@@ -52,15 +53,15 @@ object Build1 : BuildType({
         }
     }
    dependencies {
-        snapshot(Build2) {
+        snapshot(Build3) {
             reuseBuilds = ReuseBuilds.NO
         }
     }
 })
 
-object Build2 : BuildType({
-    name = "build2"
-    id("Build2customIDchanged2")
+object Build3 : BuildType({
+    name = "build3"
+    id("Build3custom")
     vcs {
         root(DslContext.settingsRoot)
     }
